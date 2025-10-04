89462a15963eae7c488f86d451e124dcd3239f56
PranavVMahajan <vinaysavi00@gmail.com>
2025-10-04 15:55:11 +0530

    global css update

--- next.config.mjs
+++ next.config.mjs
@@ -9,11 +9,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-<<<<<<< HEAD
-  devIndicators: false
-=======
   devIndicators: false, 
->>>>>>> ac5dd4c5399b1878f12036e85706cf1482885d1e
 }
 
 export default nextConfig
